5a24913751adb89ccd2bed9a12a652f125787f74
luccaparadeda <luccaparadedaprofessional@gmail.com>
2024-05-26 14:46:39 -0300

    feat: Test quase funcionando só um bugzinho e DominoGame com a nova estratégia de parar mais cedo

---
+++ Domino.java
@@ -0,0 +1,12 @@
+public class Domino {
+    int left, right;
+
+    Domino(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public String toString() {
+        return "(" + left + ", " + right + ")";
+    }
+}
--- DominoGame.java
+++ DominoGame.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class Test {
+public class DominoGame {
     public static boolean backtrack(List<Domino> dominoes, List<Domino> chain) {
         if (dominoes.isEmpty()) {
             System.out.println(chain);
@@ -32,15 +32,32 @@ public class Test {
 
     public static void main(String[] args) {
         try {
-            File file = new File("test.txt");
+            File file = new File("./test.txt");
             Scanner scanner = new Scanner(file);
             int n = scanner.nextInt();
             List<Domino> dominoes = new ArrayList<>();
+            HashMap<Integer, Integer> map = new HashMap<>();
             for (int i = 0; i < n; i++) {
                 int left = scanner.nextInt();
                 int right = scanner.nextInt();
+                if (map.containsKey(left)) {
+                    map.put(left, map.get(left) + 1);
+                } else {
+                    map.put(left, 1);
+                }
+                if (map.containsKey(right)) {
+                    map.put(right, map.get(right) + 1);
+                } else {
+                    map.put(right, 1);
+                }
                 dominoes.add(new Domino(left, right));
             }
+            map.forEach((k, v) -> {
+                if (v % 2 != 0) {
+                    System.out.println("No solution");
+                    System.exit(0);
+                }
+            });
             for (Domino domino : new ArrayList<>(dominoes)) {
                 dominoes.remove(domino);
                 if (backtrack(dominoes, new ArrayList<>(Arrays.asList(domino)))) {
