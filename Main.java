0b1060ed8a854356f0f366aba9e315c29c82675d
luccaparadeda <luccaparadedaprofessional@gmail.com>
2024-05-26 21:12:33 -0300

    refactor: added main to the top

--- Main.java
+++ Main.java
@@ -4,6 +4,27 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        try {
+            File file = new File(args[0]);
+            Scanner scanner = new Scanner(file);
+            int numberOfDominos = scanner.nextInt();
+            ArrayList<Domino> dominos = new ArrayList<>();
+            for (int i = 0; i < numberOfDominos; i++) {
+                int left = scanner.nextInt();
+                int right = scanner.nextInt();
+                dominos.add(new Domino(left, right));
+            }
+            boolean[] used = new boolean[numberOfDominos];
+            ArrayList<Domino> chain = new ArrayList<>();
+            if (!backtrack(dominos, chain, used)) {
+                System.out.println("Sem solução");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado");
+        }
+    }
+
     public static boolean backtrack(ArrayList<Domino> dominoes, ArrayList<Domino> chain, boolean[] used) {
         if (chain.size() == dominoes.size()) {
             System.out.println(chain);
@@ -30,26 +51,5 @@ public class Main {
             }
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        try {
-            File file = new File(args[0]);
-            Scanner scanner = new Scanner(file);
-            int numberOfDominos = scanner.nextInt();
-            ArrayList<Domino> dominos = new ArrayList<>();
-            for (int i = 0; i < numberOfDominos; i++) {
-                int left = scanner.nextInt();
-                int right = scanner.nextInt();
-                dominos.add(new Domino(left, right));
-            }
-            boolean[] used = new boolean[numberOfDominos];
-            ArrayList<Domino> chain = new ArrayList<>();
-            if (!backtrack(dominos, chain, used)) {
-                System.out.println("Sem solução");
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Arquivo não encontrado");
-        }
     }
 }
